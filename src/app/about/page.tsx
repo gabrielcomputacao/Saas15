0dba950d471307394140e22ea7ac90cbf35e7d4b
Gabriel <gabrielcomputacao@outlook.com>
2025-04-26 05:49:25 -0300

    feat: object title in type metadata with params default,absolute and template

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,11 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About Page",
+  title: {
+    default: "Texto que sobrescreve os textos dos filhos",
+    absolute: "texto que vai junto com o do filho colocando %s | %s",
+    template: "Texto que ignora o texto do pai e fica como sendo este",
+  },
 };
 
 export default function About() {
