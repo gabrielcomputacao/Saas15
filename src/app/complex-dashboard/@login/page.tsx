06776adac93f4db97ff48b580402a1ad9e1a7750
Gabriel <gabrielcomputacao@outlook.com>
2025-05-03 10:35:18 -0300

    feat: conditional parallel route

---
+++ src/app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,11 @@
+import { WrapperCard } from "@/components/wrapperCard/wrapperCard";
+
+export default function LoginComplex() {
+  return (
+    <div>
+      <WrapperCard>
+        <h1>Login COmplex</h1>
+      </WrapperCard>
+    </div>
+  );
+}
--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -4,26 +4,36 @@ export default function LayoutDashboard({
   children,
   notifications,
   revenue,
+  login,
 }: {
   children: React.ReactNode;
   notifications: React.ReactNode;
   revenue: React.ReactNode;
+  login: React.ReactNode;
 }) {
   /* 
         O layout recebe as rotas paralelas por props, e cada rota paralela tem seu proprio tempo de carregamento seus
         erros e suas sub-navigations
     */
 
+  const isLoggedIn = true;
+
   return (
     <div className="p-10">
       <div>{children}</div>
       <div className="flex gap-4">
-        <div>
-          <WrapperCard>{notifications}</WrapperCard>
-        </div>
-        <div>
-          <WrapperCard>{revenue}</WrapperCard>
-        </div>
+        {!isLoggedIn ? (
+          login
+        ) : (
+          <div>
+            <div className="flex gap-4">
+              <WrapperCard>{notifications}</WrapperCard>
+            </div>
+            <div>
+              <WrapperCard>{revenue}</WrapperCard>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
